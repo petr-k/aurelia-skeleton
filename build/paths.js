b11f364d777a0675ad519cbd84ac463df05b0a24
Petr Krebs <petr-k@users.noreply.github.com>
2015-03-28 23:33:54 +0100

    Initial commit
    Based on aurelia/skeleton-navigation, but stripped down heavily.

---
+++ build/paths.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+var appRoot = 'src/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  style: 'styles/**/*.css',
+  output: 'dist/'
+};
---
+++ dist/app.js
@@ -0,0 +1,17 @@
+System.register([], function (_export) {
+  var _classCallCheck, App;
+
+  return {
+    setters: [],
+    execute: function () {
+      "use strict";
+
+      _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+      App = _export("App", function App() {
+        _classCallCheck(this, App);
+      });
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO3VCQUFhLEdBQUc7Ozs7Ozs7OztBQUFILFNBQUcsa0JBQ0gsU0FEQSxHQUFHLEdBQ0E7OEJBREgsR0FBRztPQUdiIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZVJvb3QiOiIvc3JjLyJ9
